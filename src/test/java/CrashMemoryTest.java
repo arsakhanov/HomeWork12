eb16c088712c574a11bbf54343ab11d93dcbc453
arsakhanov <baki_12_95@mail.ru>
2020-09-28 20:15:04 +0300

    +class tests

--- src/test/java/CrashMemoryTest.java
+++ src/test/java/CrashMemoryTest.java
@@ -2,14 +2,16 @@ import static org.junit.Assert.*;
 
 public class CrashMemoryTest {
 
+    //Убедимся, что grabAllMemory действительно выбрасывает исключение
     @org.junit.Test(expected = OutOfMemoryError.class)
-    public void grabAllMemory() {
+    public void testGrabAllMemory() {
         CrashMemory crashMemory = new CrashMemory();
         crashMemory.grabAllMemory();
     }
 
+    //Удостоверимся, что gramPermGen действительно выбрасывает исключение
     @org.junit.Test(expected = OutOfMemoryError.class)
-    public void grabPermGen() {
+    public void testGrabPermGen() {
         CrashMemory crashMemory = new CrashMemory();
         crashMemory.grabPerpGen();
     }
